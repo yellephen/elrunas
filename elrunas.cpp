5251fab6e03893926f2573b794850ca36c11f56c
yellephen <ds_walters@hotmail.com>
2024-09-15 05:27:00 +1000

    fixed for domain

--- elrunas.cpp
+++ elrunas.cpp
@@ -6,21 +6,20 @@ int main(int argc, char* argv[])
     STARTUPINFOW Si = { sizeof(STARTUPINFOW) };
     PROCESS_INFORMATION Pi = { 0 };
 
-    if (argc < 5) {
-        printf("Usage: elrunas <username> <domain> <password> <executable>\n");
+    if (argc < 4) {
+        printf("Usage: elrunas <username>@<host|domain> <password> <executable>\n");
         return 1;
     }
 
     // Convert arguments to wide-character strings
     WCHAR wUsername[256], wDomain[256], wPassword[256], wExecutable[MAX_PATH];
     MultiByteToWideChar(CP_ACP, 0, argv[1], -1, wUsername, 256);
-    MultiByteToWideChar(CP_ACP, 0, argv[2], -1, wDomain, 256);
-    MultiByteToWideChar(CP_ACP, 0, argv[3], -1, wPassword, 256);
-    MultiByteToWideChar(CP_ACP, 0, argv[4], -1, wExecutable, MAX_PATH);
+    MultiByteToWideChar(CP_ACP, 0, argv[2], -1, wPassword, 256);
+    MultiByteToWideChar(CP_ACP, 0, argv[3], -1, wExecutable, MAX_PATH);
 
     BOOL result = CreateProcessWithLogonW(
-        wUsername, wDomain, wPassword,
-        LOGON_WITH_PROFILE, NULL, wExecutable,
+        wUsername, NULL, wPassword,
+        0, NULL, wExecutable,
         0, NULL, NULL, &Si, &Pi);
 
     if (!result)
